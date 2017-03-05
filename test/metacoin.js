5d34168012139365cad8944fe6b4114c0431c6c2
Michael Angerman <stormasm@gmail.com>
2017-03-04 18:10:48 -0800

    metacoin.js

--- test/metacoin.js
+++ test/metacoin.js
@@ -8,6 +8,7 @@ contract('MetaCoin', function(accounts) {
       assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
     });
   });
+
   it("should call a function that depends on a linked library", function() {
     var meta;
     var metaCoinBalance;
@@ -25,6 +26,7 @@ contract('MetaCoin', function(accounts) {
       assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, "Library function returned unexpected function, linkage may be broken");
     });
   });
+
   it("should send coin correctly", function() {
     var meta;
 
@@ -44,18 +46,21 @@ contract('MetaCoin', function(accounts) {
       return meta.getBalance.call(account_one);
     }).then(function(balance) {
       account_one_starting_balance = balance.toNumber();
+      console.log('1s',account_one_starting_balance);
       return meta.getBalance.call(account_two);
     }).then(function(balance) {
       account_two_starting_balance = balance.toNumber();
+      console.log('2s',account_two_starting_balance);
       return meta.sendCoin(account_two, amount, {from: account_one});
     }).then(function() {
       return meta.getBalance.call(account_one);
     }).then(function(balance) {
       account_one_ending_balance = balance.toNumber();
+      console.log('1e',account_one_ending_balance);
       return meta.getBalance.call(account_two);
     }).then(function(balance) {
       account_two_ending_balance = balance.toNumber();
-
+      console.log('2e',account_two_ending_balance);
       assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
       assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
     });
