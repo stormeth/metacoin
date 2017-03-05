5a7060615aeee3159e30d51e75051bd40d8e2938
Michael Angerman <stormasm@gmail.com>
2017-03-04 19:36:47 -0800

    mc2 working across multiple tests

--- test/mc2.js
+++ test/mc2.js
@@ -34,4 +34,71 @@ contract('Mc2', function(accounts) {
             console.log('2e', ac2_end);
         });
     });
+
+    it("t2", function() {
+        var meta;
+
+        // Get initial balances of first and second account.
+        var ac1 = accounts[0];
+        var ac2 = accounts[1];
+
+        var ac1_start;
+        var ac2_start;
+        var ac1_end;
+        var ac2_end;
+
+        var amount = 10;
+
+        return MetaCoin.deployed().then(function(instance) {
+            meta = instance;
+            return meta.sendCoin(ac2, amount);
+        }).then(function() {
+            return meta.sendCoin(ac2, 20);
+        }).then(function() {
+            return meta.sendCoin(ac2, 30);
+        }).then(function() {
+            return meta.getBalance.call(ac1);
+        }).then(function(balance) {
+            ac1_end = balance.toNumber();
+            console.log('1e', ac1_end);
+            return meta.getBalance.call(ac2);
+        }).then(function(balance) {
+            ac2_end = balance.toNumber();
+            console.log('2e', ac2_end);
+        });
+    });
+
+    it("t3", function() {
+        var meta;
+
+        // Get initial balances of first and second account.
+        var ac1 = accounts[0];
+        var ac2 = accounts[1];
+
+        var ac1_start;
+        var ac2_start;
+        var ac1_end;
+        var ac2_end;
+
+        var amount = 10;
+
+        return MetaCoin.deployed().then(function(instance) {
+            meta = instance;
+            return meta.sendCoin(ac2, amount);
+        }).then(function() {
+            return meta.sendCoin(ac2, 40);
+        }).then(function() {
+            return meta.sendCoin(ac2, 60);
+        }).then(function() {
+            return meta.getBalance.call(ac1);
+        }).then(function(balance) {
+            ac1_end = balance.toNumber();
+            console.log('1e', ac1_end);
+            return meta.getBalance.call(ac2);
+        }).then(function(balance) {
+            ac2_end = balance.toNumber();
+            console.log('2e', ac2_end);
+        });
+    });
+
 });
